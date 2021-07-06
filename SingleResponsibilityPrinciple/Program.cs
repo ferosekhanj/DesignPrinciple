8780c9d59813e15fd649b855440965b71d1f8d8e
Ferose khan, Jeberla <j.ferosekhan@siemens-healthineers.com>
2021-07-06 12:52:45 +0530

    SRP step 1

---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleResponsibilityPrinciple
+{
+    class Milkman
+    {
+        List<int> milkOrders = new();
+        public Milkman()
+        {
+        }
+
+        public void SetOrder(List<int> dayOrder)
+        {
+            milkOrders.AddRange(dayOrder); 
+        }
+        public void DeliverMilk()
+        {
+            for (var i = 0; i <milkOrders.Count; i++)
+            {
+                System.Console.WriteLine($"house {i} => {milkOrders[i]}");
+            }
+            System.Console.WriteLine("Completed.");
+        }
+    } 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Milkman m1 = new Milkman();
+            m1.SetOrder(new() {1,2,1,2,1});
+            m1.DeliverMilk();
+        }
+    }
+}
---
+++ SingleResponsibilityPrinciple/README.md
@@ -0,0 +1,10 @@
+# Milkman
+
+We would like to help our milkman in delivery.
+
+# Features
+
+## Step 1
+- Each house in the street provides the number of milk packs needed.
+- Our Milkman in the morning delivers the order to each house.
+
