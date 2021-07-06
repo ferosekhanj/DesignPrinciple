9af06ae0470276179fc1a3e641d7d5b8f84ffbb2
Ferose khan, Jeberla <j.ferosekhan@siemens-healthineers.com>
2021-07-06 13:05:11 +0530

    SRP Step 2

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 
 namespace SingleResponsibilityPrinciple
 {
+    enum MilkType
+    {
+        Cow,
+        Goat,
+        Soya
+    }
     class Milkman
     {
         List<int> milkOrders = new();
+        List<MilkType> milkOrderTypes = new();
         public Milkman()
         {
         }
 
-        public void SetOrder(List<int> dayOrder)
+        public void SetOrder(List<int> dayOrder,List<MilkType> dayOrderType)
         {
-            milkOrders.AddRange(dayOrder); 
+            milkOrders.AddRange(dayOrder);
+            milkOrderTypes.AddRange(dayOrderType);
         }
         public void DeliverMilk()
         {
             for (var i = 0; i <milkOrders.Count; i++)
             {
-                System.Console.WriteLine($"house {i} => {milkOrders[i]}");
+                System.Console.WriteLine($"house {i} => {milkOrderTypes[i]},{milkOrders[i]}");
             }
             System.Console.WriteLine("Completed.");
         }
@@ -28,7 +36,7 @@ namespace SingleResponsibilityPrinciple
         static void Main(string[] args)
         {
             Milkman m1 = new Milkman();
-            m1.SetOrder(new() {1,2,1,2,1});
+            m1.SetOrder(new() {1,2,1},new(){MilkType.Cow,MilkType.Goat,MilkType.Goat});
             m1.DeliverMilk();
         }
     }
--- SingleResponsibilityPrinciple/README.md
+++ SingleResponsibilityPrinciple/README.md
@@ -1,3 +1,5 @@
+# Single Responsibility Principle
+
 # Milkman
 
 We would like to help our milkman in delivery.
@@ -8,3 +10,8 @@ We would like to help our milkman in delivery.
 - Each house in the street provides the number of milk packs needed.
 - Our Milkman in the morning delivers the order to each house.
 
+## Step 2
+- We are delivering 3 different type of milk
+- Cow, Goat & Soya
+- House can choose the milk type
+  
