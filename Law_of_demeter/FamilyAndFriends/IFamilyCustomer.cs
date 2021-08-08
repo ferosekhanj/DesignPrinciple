fe8dafc9cacde624aa181a1913068f806848291a
Ferose khan, Jeberla <j.ferosekhan@siemens-healthineers.com>
2021-08-08 20:03:04 +0530

    02.2_Law_of_demeter

--- Law_of_demeter/FamilyAndFriends/IFamilyCustomer.cs
+++ Law_of_demeter/FamilyAndFriends/IFamilyCustomer.cs
@@ -5,9 +5,9 @@ namespace FamilyAndFriends
 {
     interface IFamilyCustomer
     {
-        Address Address { get; init; }
         Name FatherName { get; init; }
         Name MotherName { get; init; }
+        Address Address { get; init; }
         IReadOnlyList<Child> Children { get; }
         int RequestPayment(string item, int price);
     }
