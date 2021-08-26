bc9ab8f302fabed03ba67a863ee1b33a4991f5d4
Ferose khan, Jeberla <j.ferosekhan@siemens-healthineers.com>
2021-08-26 16:56:14 +0530

    02_LSP

---
+++ Liskov_Substitution_Principle/LiskovSubstitutionPrinciple/LiskovSubstitutionPrinciple/Actors/Visitor.cs
@@ -0,0 +1,22 @@
+﻿using LiskovSubstitutionPrinciple.System;
+using System;
+
+namespace LiskovSubstitutionPrinciple.Actors
+{
+    class Visitor : Staff
+    {
+        public Visitor(string name)
+        {
+            RollNumber = -1;
+            IsActive = true;
+            Name = name;
+        }
+
+        public override void ChargeExpense(decimal amount)
+        {
+            //do nothing
+            Console.WriteLine("I don't have a means to pay.");
+        }
+
+    }
+}
