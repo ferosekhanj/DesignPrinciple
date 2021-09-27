4c73c7f73c3d68fb5d0bbf4ae6b20f01668843a0
Ferose khan, Jeberla <j.ferosekhan@siemens-healthineers.com>
2021-09-27 11:55:21 +0530

    02_Interface_Segregation_Principle

--- Interface_Segregation_Principle/Program.cs
+++ Interface_Segregation_Principle/Program.cs
@@ -4,6 +4,7 @@ namespace Interface_Segregation_Principle
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -14,6 +15,27 @@ namespace Interface_Segregation_Principle
 
             WithdrawalTeller withdrawClerk = teller as WithdrawalTeller;
             withdrawClerk.Withdraw(new Check(DateTime.Now,"Jane","1234",10));
+
+            // Apply for a kids passport
+            var form = new PassportForm
+            {
+                Name = "Khan",
+                DateOfBirth = DateTime.Now,
+                IsMinor = true,
+                ParentPassport = "ABC123",
+                BiometricsRequired = false,
+                IsGovernmentServant = false,
+                IsSeniorCitizen = false,
+                PlaceOfBirth = "Bangalore",
+                PoliceVerificationDetails = null,
+                PostalAddress = "1, one street, Pin-5600021",
+                PostalCharge = 10,
+                ServiceLocation = null
+            };
+            PassportOfficer officer = new PassportOfficer("Tom");
+            officer.Apply(form);
+            //officer.SubmitBiometrics(form); throws error
+            officer.CollectAcknowledgement(form);
 
         }
     }
