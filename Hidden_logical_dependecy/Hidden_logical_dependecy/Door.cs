340d8f31fbbd0940ab8fb317f4534cae2fe66859
Ferose Khan, Jeberla <j.ferosekhan@siemens-healthineers.com>
2023-06-26 17:05:48 +0530

    08.2_Hidden_logical_dependency

--- Hidden_logical_dependecy/Hidden_logical_dependecy/Door.cs
+++ Hidden_logical_dependecy/Hidden_logical_dependecy/Door.cs
@@ -7,12 +7,22 @@
     {
         public bool Locked { get; private set; }
 
+        public bool ChildLock { get; set; }
+
         public Door()
         {
             Locked = true;
+            ChildLock = false;
         }
 
-        public void Open() => Locked = false;
+        public void Open(bool fromInside)
+        {
+            if(fromInside && ChildLock)
+            {
+                return;
+            }
+            Locked = false;
+        }
         public void Close() => Locked = true;
 
     }
--- Hidden_logical_dependecy/Hidden_logical_dependecy/Program.cs
+++ Hidden_logical_dependecy/Hidden_logical_dependecy/Program.cs
@@ -9,9 +9,22 @@
             Car honda = new Car();
             Console.WriteLine($"Door => Locked={honda.Doors[0].Locked}");
 
-            honda.Doors[0].Open();
+            // enable child lock
+            honda.Doors[0].ChildLock = true;
+
+            // Open from the outside
+            honda.Doors[0].Open(false);
             Console.WriteLine($"Door.Open() => Locked={honda.Doors[0].Locked}");
 
+            // Close door
+            honda.Doors[0].Close();
+            Console.WriteLine($"Door.Close() => Locked={honda.Doors[0].Locked}");
+
+            // Open from the inside
+            honda.Doors[0].Open(true);
+            Console.WriteLine($"Door.Open() => Locked={honda.Doors[0].Locked}");
+
+            // Close door
             honda.Doors[0].Close();
             Console.WriteLine($"Door.Close() => Locked={honda.Doors[0].Locked}");
         }
